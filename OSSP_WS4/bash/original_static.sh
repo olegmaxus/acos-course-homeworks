286d9b6e96132bb5fdd267f7465bdb4f5ab0fd65
Oleg <55272140+OlegMalchenko@users.noreply.github.com>
2021-04-15 01:43:28 +0300

    Update original_static.sh

--- OSSP_WS4/bash/original_static.sh
+++ OSSP_WS4/bash/original_static.sh
@@ -3,5 +3,5 @@ gcc -c fred.c john.c
 ar crv libfoo.a fred.o john.o
 gcc -c program_for_static.c
 gcc -o program_static program_for_static.o fred.o john.o -lm
-gcc -o program_static program_for_static.o -L. -lfoo -lm
+gcc -o program_static program_for_static.o -L. -lfoo
 ./program_static
