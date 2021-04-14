6196b922a22a33fd1ad2f063370cb0e9a659f2b3
Oleg <55272140+OlegMalchenko@users.noreply.github.com>
2021-04-15 01:25:48 +0300

    Update improved_dynamic.sh

--- OSSP_WS4/bash/improved_dynamic.sh
+++ OSSP_WS4/bash/improved_dynamic.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-gcc -Wall -DCOMPILETIME -c -fPIC bill.c sam.c -lm
+gcc -Wall -DLOADTIME -c -fPIC bill.c sam.c -lm
 gcc -Wall -shared -o libfoo.so bill.o sam.o
 gcc -Wall -o program_dynamic_improved program_for_dynamic.c -lfoo -lm -L.
 export LD_LIBRARY_PATH=.
